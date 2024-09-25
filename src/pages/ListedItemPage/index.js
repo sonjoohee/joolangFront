926176d09947f6e343fa1ab6f417577f1a511db0
Minchaez <rlawlsdl0730@gmail.com>
2024-09-25 21:37:05 +0900

    reset

--- src/pages/ListedItemPage/index.js
+++ src/pages/ListedItemPage/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return (
-    <div>
-      <p>hi</p>
-      <p>hi2</p>
-      <p>hi3</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default index;
