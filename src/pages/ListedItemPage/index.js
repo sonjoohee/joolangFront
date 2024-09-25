33abb42a3a71150cb9c7ea93eeca094362343495
Minchaez <rlawlsdl0730@gmail.com>
2024-09-25 21:20:36 +0900

    hi3

--- src/pages/ListedItemPage/index.js
+++ src/pages/ListedItemPage/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
     <div>
       <p>hi</p>
       <p>hi2</p>
+      <p>hi3</p>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
