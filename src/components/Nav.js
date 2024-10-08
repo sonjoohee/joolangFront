3d2526bf1a4a123c6d72f761c5c5d69e2c802556
sonjoohee <90837976+sonjoohee@users.noreply.github.com>
2024-10-08 18:57:32 +0900

    ustate

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 import {useNavigate } from 'react-router-dom';
